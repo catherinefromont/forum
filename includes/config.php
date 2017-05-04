8b21beb30d730309d706f701306bcf4ee6b05c6b
catherinefromont <catherinefromont@Catherines-Air.atcltd.atc.org.nz>
2017-05-04 16:24:47 +1200

    DONE

--- includes/config.php
+++ includes/config.php
@@ -4,11 +4,20 @@ date_default_timezone_set('Pacific/Auckland');
 
 require 'includes/functions.php';
 
-
+// LOCAL
+// 
 $host = 'localhost';
 $user = 'root';
 $pass = 'root';
 $database = 'forum';
+
+
+// PRODUCTION
+// 
+// $host = 'sql308.host.org.nz';
+// $user = 'hostnz_20050733';
+// $pass = 'password1';
+// $database = 'hostnz_20050733_cat';
 
 
 $dbh = connectDatabase($host,$database,$user,$pass);
